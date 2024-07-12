0b266da9baa22cc5e64ed1426996dd33b7ed72aa
fabio817 <130922948+fabio817@users.noreply.github.com>
2024-07-11 23:39:44 -0300

    App curso Gas x Eta

---
+++ app/src/main/java/com/fabio/app/gaseta/view/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.fabio.app.gaseta.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fabio.app.gaseta.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        cumutarTelaSplash();
+
+    }
+
+    private void cumutarTelaSplash() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent telaPrincipal = new Intent(SplashActivity.this, GasetaActivity.class);
+                startActivity(telaPrincipal);
+                finish();
+            }
+        },TIME_OUT_SPLASH);
+
+
+    }
+}
